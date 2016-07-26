77a4ac101f394e2802459942bb56ab59739367a0
Lydia Cheah <cheahlizuan@gmail.com>
2016-07-26 20:00:11 +0800

    fix syntax error in payment controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -12,16 +12,13 @@ class PaymentsController < ApplicationController
 	  		amount: @reservation.listing.price,
 	  		payment_method_nonce: params[:payment_method_nonce]
 	  	)
-	  	if @result.success?
-	  		@reservation.paid!
-	  		Payment.create(user_id: current_user.id, reservation_id: @reservation.id, last_four: @result.transaction.credit_card_details.last_4, ref_no: @result.transaction.id, success: true)
-	  		redirect_to listing_path(@reservation.listing.id), flash:{success: "Your payment is successful! The host has been notified of your reservation!"}
-	  	else
-	  		Payment.create(user_id: current_user.id, reservation_id: @reservation.id, last_four: @result.transaction.credit_card_details.last_4, ref_no: @result.transaction.id, success: false)
-	  		redirect_to new_payment_path(@reservation.id), flash:{danger: "Payment failed. Please try again."}
-	  	end
-	  else
-	  	redirect_to new_payment_path(@reservation.id), flash:{danger: "Invalid credit card. Please try again."}
-	  end
+  	if @result.success?
+  		@reservation.paid!
+  		Payment.create(user_id: current_user.id, reservation_id: @reservation.id, last_four: @result.transaction.credit_card_details.last_4, ref_no: @result.transaction.id, success: true)
+  		redirect_to listing_path(@reservation.listing.id), flash:{success: "Your payment is successful! The host has been notified of your reservation!"}
+  	else
+  		Payment.create(user_id: current_user.id, reservation_id: @reservation.id, last_four: @result.transaction.credit_card_details.last_4, ref_no: @result.transaction.id, success: false)
+  		redirect_to new_payment_path(@reservation.id), flash:{danger: "Payment failed. Please try again."}
+  	end
   end
 end
